b8b3e282105d8fbedd2004ba81a988f71e73c931
dicksonmuli <dicksonmully6@gmail.com>
2017-04-25 16:51:32 +0300

    testing the volume of a cube

---
+++ src/main/java/Cube.java
@@ -0,0 +1,16 @@
+public class Cube {
+	private Rectangle mFace;
+
+	public Cube(Rectangle rectangle) {
+		mFace = rectangle;
+	}
+
+	public Rectangle getFace() {
+		return mFace;
+	}
+
+	public int volume() {
+		int length = mFace.getLength();
+		return length * length * length;
+	}
+}
