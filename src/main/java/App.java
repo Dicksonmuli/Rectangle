4877bd049bcd81334624c320d1a6781005fdfd95
dicksonmuli <dicksonmully6@gmail.com>
2017-04-26 06:41:39 +0300

    cube surface area and volume

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,5 +9,28 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+		get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+		// route rectangle
+		get("/rectangle", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  int length = Integer.parseInt(request.queryParams("length"));
+  int width = Integer.parseInt(request.queryParams("width"));
+
+  Rectangle myRectangle = new Rectangle(length, width);
+  model.put("myRectangle", myRectangle);
+
+  if (myRectangle.isSquare()) {
+    Cube myCube = new Cube(myRectangle);
+    model.put("myCube", myCube)
+  }
+
+  model.put("template", "templates/rectangle.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
   }
 }
